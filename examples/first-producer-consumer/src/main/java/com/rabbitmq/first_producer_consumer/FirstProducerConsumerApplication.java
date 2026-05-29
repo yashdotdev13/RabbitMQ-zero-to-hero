8c5d4e8891ab6257e43fcb9575b4fea3b4dacb49
98001yash <yashchauhan.gaya@gmail.com>
2026-05-30 00:10:20 +0530

    initializ a maven spring boot project under example folder to understand the chapter 5

---
+++ examples/first-producer-consumer/src/main/java/com/rabbitmq/first_producer_consumer/FirstProducerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.first_producer_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProducerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProducerConsumerApplication.class, args);
+	}
+
+}
---
+++ examples/first-producer-consumer/src/test/java/com/rabbitmq/first_producer_consumer/FirstProducerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.first_producer_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProducerConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
