ff1dd307365c7ac877a5ebb65482f69d3fd2c672
98001yash <yashchauhan.gaya@gmail.com>
2026-06-05 00:43:43 +0530

    added the OrderCreatedEvent and RabbitMQConfig class

---
+++ projects/ecommerce-event-driven-system/order-service/src/main/java/com/rabbitmq/order_service/config/RabbitMQConfig.java
@@ -0,0 +1,35 @@
+package com.rabbitmq.order_service.config;
+
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String ORDER_QUEUE = "order.created.queue";
+    public static final String ORDER_EXCHANGE = "order.exchange";
+    public static final String ORDER_ROUTING_KEY = "order.created";
+
+    @Bean
+    public Queue orderQueue(){
+        return new Queue(ORDER_QUEUE);
+    }
+
+    @Bean
+    public DirectExchange orderExchange() {
+        return new DirectExchange(ORDER_EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(orderQueue())
+                .to(orderExchange())
+                .with(ORDER_ROUTING_KEY);
+    }
+}
